752cc8d6207f1957b938ab3601a60bcdc0d02fcd
Kingslayer <sirjanssk2933@protonmail.ch>
2022-01-17 17:54:56 +0530

    Solution for systemin_systemout

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 list of questions:
 
-+ [ ] [Hello](https://www.hackerrank.com/challenges/welcome-to-java/problem)
++ [ ] [Hello:](https://www.hackerrank.com/challenges/welcome-to-java/problem)
++ [ ] [systemin_systemout:](https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem)
---
+++ systemin_systemout.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class systemin_systemout {
+
+    public static void main(String[] args) {
+        Scanner f=new Scanner(System.in);
+        int u=f.nextInt();
+        int c=f.nextInt();
+        int k=f.nextInt();
+        System.out.println(u+"\n"+c+"\n"+k);
+    }
+}
