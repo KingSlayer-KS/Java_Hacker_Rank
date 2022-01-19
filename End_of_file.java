76075137f45af19264521708efd0bacffed762c2
Kingslayer <sirjanssk2933@protonmail.ch>
2022-01-19 13:42:01 +0530

    Solution for End-of-file

--- End_of_file.java
+++ End_of_file.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class End_of_file {
     public static void main(String[] args) {
         Scanner f=new Scanner(System.in);
-        Integer u= 0;
+        Integer u = 0;
         while (f.hasNext()){
             u+=1;
-            f.nextLine();
             String c=f.nextLine();
             System.out.println(u+" "+c);
         }
