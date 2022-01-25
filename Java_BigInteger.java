fa988176a7da0e20ef7ada19c5d94f4048bf1649
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-01-25 14:46:32 +0530

    Solution for Java_BigInteger

--- Java_BigInteger.java
+++ Java_BigInteger.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Java_BigInteger {
     public static void main(String[] args) {
-        Scanner f= new Scanner(System.in);
-        BigInteger u=f.nextBigInteger();
-        BigInteger c=f.nextBigInteger();
-        f.close();
+        Scanner ff= new Scanner(System.in);
+        BigInteger u=ff.nextBigInteger();
+        BigInteger c=ff.nextBigInteger();
+        ff.close();
         System.out.println(u.add(c)+"\n"+u.multiply(c));
     }
 }
