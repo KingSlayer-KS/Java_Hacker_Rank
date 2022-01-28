40ca8252fdcfafe39be4bfd92419369231686a2c
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-01-28 20:25:10 +0530

    Solution for Java_List

--- int_to_string.java
+++ int_to_string.java
@@ -1,18 +1,15 @@
 import java.util.*;
-import java.security.*;
+
 public class int_to_string {
     public static void main(String[] args) {
 
-        DoNotTerminate.forbidExit();
-
         try {
             Scanner in = new Scanner(System.in);
-            int n = in .nextInt();
+            int n = in.nextInt();
             in.close();
-            //String s=???; Complete this line below
+            // String s=???; Complete this line below
             String s = Integer.toString(n);
-            //Write your code here
-
+            // Write your code here
 
             if (n == Integer.parseInt(s)) {
                 System.out.println("Good job");
@@ -25,24 +22,12 @@ public class int_to_string {
     }
 }
 
- //The following class will prevent you from terminating the code using exit(0)!
- class DoNotTerminate {
+// The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
 
-     public static class ExitTrappedException extends SecurityException {
+    public static class ExitTrappedException extends SecurityException {
 
-         private static final long serialVersionUID = 1;
-     }
+        private static final long serialVersionUID = 1;
+    }
 
-     public static void forbidExit() {
-         final SecurityManager securityManager = new SecurityManager() {
-             @Override
-             public void checkPermission(Permission permission) {
-                 if (permission.getName().contains("exitVM")) {
-                     throw new ExitTrappedException();
-                 }
-             }
-         };
-         System.setSecurityManager(securityManager);
-
-     }
- }
+}
