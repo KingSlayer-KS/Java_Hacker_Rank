ecac52a9165c8c66dfd1a6306f6a3513fc8b80dc
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-01-31 13:11:39 +0530

    Solution for Java Sort

---
+++ Java_Sort.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Student {
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+// Complete the code
+public class Java_Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCases = Integer.parseInt(sc.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while (testCases > 0) {
+            int id = sc.nextInt();
+            String fname = sc.next();
+            double cgpa = sc.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+        Collections.sort(studentList, Comparator.comparing(Student::getCgpa).reversed().thenComparing(Student::getFname)
+                .thenComparing(Student::getId));
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+        sc.close();
+    }
+}
--- README.md
+++ README.md
@@ -63,5 +63,7 @@ list of questions:
 
 + [ ] [Java_Hashset:](https://www.hackerrank.com/challenges/java-hashset/problem)
 
++ [ ] [Java_Sort:](https://www.hackerrank.com/challenges/java-sort/problem)
+
 + [ ] [Java_Varargs__Simple_Addition:](https://www.hackerrank.com/challenges/java-list/problem)
 
